edf2a9db7f3eab9c9cb087140595083d4e7464c6
GreyRobin <86514645+GreyRobin@users.noreply.github.com>
2021-09-22 16:49:39 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var bomb
+var ground
+var button
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  bomb = new Bomb(200,100,80,80)
+  ground = new Ground(200,350,width,height/3)
+  button = createButton("click to shoot")
+  button.position(350,200)
+  button.class("button")
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  bomb.show()
+  ground.show()
+  drawSprites()
+}
+
+function shoot(){
+  Matter.Body.applyForce(bomb.body,{x:0,y:0},{x:-0.05,y:0})
+}
